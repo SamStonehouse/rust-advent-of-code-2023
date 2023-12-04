cbd8926b8f95bd3aef4d13da764cb654ba1b2062
Sam Stonehouse <sgstonehouse@googlemail.com>
2023-12-04 20:06:29 +0000

    Added scaffold for day 4

---
+++ src/day_four.rs
@@ -0,0 +1,11 @@
+use crate::inputs;
+
+pub(crate) fn part_one() {
+    println!("Day Four, Part One");
+    let lines = inputs::read_inputs_from_file("./inputs/day_four.txt").unwrap();
+}
+
+pub(crate) fn part_two() {
+    println!("Day Four, Part Two");
+    let lines = inputs::read_inputs_from_file("./inputs/day_four.txt").unwrap();
+}
